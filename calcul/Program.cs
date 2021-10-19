89ec3c079eb28d488af11af9e121b66b7d7de1a4
COUSOULO <COUSOULO@DESKTOP-KQ4U65N>
2021-10-19 16:33:52 +0200

    Ajoutez des fichiers projet.

---
+++ calcul/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace calcul
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            // variables 
+            Random rand = new Random(); // outil de génération de nombre aléatoire
+            int val1, val2; // mémorisation de nombres aléatoires
+            int solution; // calcul de la solution
+            int reponse = 0; // saisie de la réponse de l'utilisateur
+            string choix; // saisie du choix de l'utilsiateur
+            bool correct; // mémorise si la saisie est un entier
+
+            // boucle sur le menu
+            choix = "1";
+            while (choix != "0")
+            {
+                // affiche le menu et saisi le choix
+                Console.WriteLine("Addition ....................... 1");
+                Console.WriteLine("Multiplication ................. 2");
+                Console.WriteLine("Quitter ........................ 0");
+                Console.Write("Choix :                          ");
+                choix = Console.ReadLine();
+                // traitement des choix
+                switch (choix)
+                {
+                    case "1": // addition
+                        val1 = rand.Next(1, 10);
+                        val2 = rand.Next(1, 10);
+                        // saisie de la réponse
+                        correct = false;
+                        while (!correct)
+                        {
+                            try
+                            {
+                                Console.Write(val1 + " + " + val2 + " = ");
+                                reponse = int.Parse(Console.ReadLine());
+                                correct = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Saisissez un entier");
+                            }
+                        }
+                        // comparaison avec la réponse
+                        solution = val1 + val2;
+                        if (reponse == solution)
+                        {
+                            Console.WriteLine("Bravo !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Faux : " + val1 + " + " + val2 + " = " + solution);
+                        }
+                        break;
+                    case "2": // multiplication
+                        val1 = rand.Next(1, 10);
+                        val2 = rand.Next(1, 10);
+                        // saisie de la réponse
+                        correct = false;
+                        while (!correct)
+                        {
+                            try
+                            {
+                                Console.Write(val1 + " x " + val2 + " = ");
+                                reponse = int.Parse(Console.ReadLine());
+                                correct = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Saisissez un entier");
+                            }
+                        }
+                        // comparaison avec la réponse
+                        solution = val1 * val2;
+                        if (reponse == solution)
+                        {
+                            Console.WriteLine("Bravo !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Faux : " + val1 + " x " + val2 + " = " + solution);
+                        }
+                        break;
+                    case "0": // demande de fin de programme
+                        break;
+                    default: // autre valeur donc erreur de saisie
+                        Console.WriteLine("Erreur de saisie");
+                        break;
+                }
+            }
+        }
+    }
+
+}
+
+
+    
